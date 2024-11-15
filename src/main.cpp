2b2579fc3c2d7e95a2f5fed4035eb137a7cfcde8
Marcus <marcusyeww@gmail.com>
2024-11-15 16:15:35 +0800

    bluetooth control with analog stick

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 #include "car.h"
 
 
+char bt;
+
 void setup () {
-// put your setup code here, to run once:
+/*Initialize Class*/
+  Serial.begin (9600); 
+
+/*Pin Mode*/
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
@@ -13,11 +18,11 @@ void setup () {
 }
 
 void loop() {
-// put your main code here, to run repeatedly:
+
   /*forward(100);
-  delay(3000);
+  delay(1000);
   backward(100);
-  delay(3000);*/
+  delay(3000);
   turn(100,255); //turn left
   delay(3000);
   turn(200,100);
@@ -25,6 +30,42 @@ void loop() {
   turn(-100,100);
   delay(3000);
   turn(100,-100);
-  delay(3000);
+  delay(3000);*/
+bt = Serial.read();
 
+  if(bt == 'F'){  //striaght forward
+    forward(150);
+  }
+  else if(bt == 'B'){  //straight backwards
+    backward(150);
+  }
+  else if(bt == 'L'){  //left on itself
+    turn(-150, 150);
+  }
+  else if(bt == 'R'){  //right on itself
+    turn(150, -150);
+  }
+
+  else if(bt == 'Q'){   //diagonal left top
+    turn(100,150);
+  }
+
+
+  else if(bt == 'E'){   //diagonal right top 
+    turn(150,100);
+  }
+
+
+  else if(bt == 'Z'){  //diagonal left back
+    turn(-100,-150);
+  }
+
+
+  else if(bt == 'C'){  //diagonal right back
+    turn(-150,-100);
+  }
+
+  else if(bt == 'S'){  //stop 
+    stop(0);
+  }
 }
